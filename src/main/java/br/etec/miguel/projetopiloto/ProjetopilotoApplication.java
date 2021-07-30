3a4d219c060a59bfce2a8a96ba7ea36aaa4ea7dd
Miguel Jose Albano da Silva <61022078+josemiguel2004@users.noreply.github.com>
2021-07-30 15:58:55 -0300

    Initial commit

---
+++ src/main/java/br/etec/miguel/projetopiloto/ProjetopilotoApplication.java
@@ -0,0 +1,13 @@
+package br.etec.miguel.projetopiloto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetopilotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopilotoApplication.class, args);
+	}
+
+}
